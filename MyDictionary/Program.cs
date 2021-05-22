03cba887783b739a78868758c0084fed4d25147c
Mahmut ÇAVUŞ <somber08@hotmail.com>
2021-05-22 14:10:11 +0300

    Add project files.

---
+++ MyDictionary/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyList<string, int> myList = new MyList<string, int>();
+            myList.Add("mustafa", 111);
+        }
+    }
+}
